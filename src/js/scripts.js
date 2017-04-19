0618c7037c19999d92b9975b20314ea7083184c5
Mikhail Ragulin <anarchy16@inbox.ru>
2017-04-19 13:30:20 +0600

    main-slider, categories, feedbacks

---
+++ src/js/scripts.js
@@ -0,0 +1,12 @@
+(function ($) {
+    $(document).ready(function () {
+ 
+        //  Слайдер
+        $('.main-slider').slick({
+            autoplay: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false
+        });
+    });
+})(jQuery);
